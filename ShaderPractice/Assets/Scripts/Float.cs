8f6149636e4a8b8ec5eb7ab5b73ce81f550f3e33
Cameron <cameron.meyer02@gmail.com>
2020-02-28 01:34:30 -0600

    Good Spirit Orb
    Added blob that follows camera

---
+++ ShaderPractice/Assets/Scripts/Float.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Float : MonoBehaviour
+{
+    [SerializeField] float sinMagnitude;
+
+    private Vector3 startPos;
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = startPos + new Vector3(0, sinMagnitude* Mathf.Sin(Time.time) * Time.deltaTime, 0);
+            //new Vector3(startPos.x, Mathf.Sin(Time.time) * Time.deltaTime, startPos.z);
+    }
+}
