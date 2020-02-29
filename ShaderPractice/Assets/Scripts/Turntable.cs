ffe905483ede6254cacaf98c01737223ca7ed649
Cameron <cameron.meyer02@gmail.com>
2020-02-29 14:48:55 -0600

    Spirit Orb and Holo Touch Ups
    Also added a bulbasaur model

---
+++ ShaderPractice/Assets/Scripts/Turntable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turntable : MonoBehaviour
+{
+    [SerializeField] float rotSpeed;
+    
+    void FixedUpdate()
+    {
+        transform.Rotate(0.0f, rotSpeed, 0.0f);
+    }
+}
