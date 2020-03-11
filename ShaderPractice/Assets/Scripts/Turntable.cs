5b4978abd7bd9e0ebef6489c116c852979a69f1d
Cameron <cameron.meyer02@gmail.com>
2020-03-11 00:48:37 -0500

    Toon Shader Brush Strokes + Link Materials Update
    Also began testing some gamelab outline shader stuff for Andrew in here

--- ShaderPractice/Assets/Scripts/Turntable.cs
+++ ShaderPractice/Assets/Scripts/Turntable.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class Turntable : MonoBehaviour
 {
     [SerializeField] float rotSpeed;
+    [SerializeField] bool rotOnYAxis = true;
+    [SerializeField] bool rotOnXAxis = false;
     
     void FixedUpdate()
     {
-        transform.Rotate(0.0f, rotSpeed, 0.0f);
+        if (rotOnYAxis)
+            transform.Rotate(0.0f, rotSpeed, 0.0f);
+        else if (rotOnXAxis)
+            transform.Rotate(rotSpeed, 0.0f, 0.0f);
+        else
+            transform.Rotate(0.0f, 0.0f, rotSpeed);
     }
 }
